9f3aa3774c7baf61f9d8ae73af1fbb5d58de79d8
Max <Bruce.97@mail.ru>
2023-04-15 09:41:06 +0300

    add task4.py

---
+++ task4.py
@@ -0,0 +1,10 @@
+numOfCranes = 24
+
+madeByKatya = numOfCranes * 2/3
+madeByPetya = madeByKatya / 4
+madeBySergey = madeByKatya / 4
+
+print(f"Общее количество: {numOfCranes}")
+print(f"Сделано Катей: {madeByKatya}")
+print(f"Сделано Петей: {madeByPetya}")
+print(f"Сделано Сергеем: {madeBySergey}")
